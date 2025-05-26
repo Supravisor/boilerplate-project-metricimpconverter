89425dc1a2a211ac7d837099841f2cad4b587b1e
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-05-26 03:54:20 +0000

    Unit 1 of 16

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,4 +6,12 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
 
+    test("valid whole number input", () => {
+        assert.strictEqual(
+          convertHandler.getNum("2"),
+          2,
+          "read valid whole number input"
+        );
+      });
+  
 });
