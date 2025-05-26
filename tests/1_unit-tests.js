d9dd767e8abd2aef94c337ea7a7315063ceb7ade
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-05-26 04:15:49 +0000

    Units 2 to 9

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -22,4 +22,110 @@ suite('Unit Tests', function(){
         );
       });
 
+      test("valid fractional input", () => {
+        assert.strictEqual(
+          convertHandler.getNum("1/2"),
+          0.5,
+          "read valid fractional input"
+        );
+      });
+
+      test("valid fractional input with decimal", () => {
+        assert.strictEqual(
+          convertHandler.getNum("0.4/0.8"),
+          0.5,
+          "read valid fractional input with decimal"
+        );
+      });
+
+      test("invalid double fraction input", () => {
+        assert.strictEqual(
+          convertHandler.getNum("invalid number"),
+          "invalid number",
+          "error message for invalid double fraction input"
+        );
+      });
+
+      test("no numeric input", () => {
+        assert.strictEqual(
+          convertHandler.getNum("1"),
+          1,
+          "1 unit for no numeric input"
+        );
+      });
+
+      test("valid input units", () => {
+        assert.equal(
+          convertHandler.getUnit("gal"),
+          "gal",
+          "read gal"
+        );
+        assert.strictEqual(
+          convertHandler.getUnit("L"),
+           "L",
+           "read L"
+        );
+        assert.strictEqual(
+          convertHandler.getUnit("mi"),
+          "mi",
+          "read mi"
+        );
+        assert.strictEqual(
+          convertHandler.getUnit("km"),
+          "km",
+          "read km"
+        );
+        assert.strictEqual(
+          convertHandler.getUnit("lbs"),
+          "lbs",
+          "read lbs"
+        );
+        assert.strictEqual(
+          convertHandler.getUnit("kg"),
+          "kg",
+          "read kg"
+        );
+      });
+
+      test("invalid input unit", () => {
+        assert.strictEqual(
+          convertHandler.getUnit("2gak"),
+          "invalid unit",
+          "error message for invalid input unit"
+        );
+      });
+
+      test("return unit for valid input unit", () => {
+        assert.strictEqual(
+          convertHandler.getReturnUnit("gal"),
+          "L",
+          "return L for gal"
+        );
+        assert.strictEqual(
+          convertHandler.getReturnUnit("L"),
+          "gal",
+          "return gal for L"
+        );
+        assert.strictEqual(
+          convertHandler.getReturnUnit("mi"),
+          "km",
+          "return km for mi"
+        );
+        assert.strictEqual(
+          convertHandler.getReturnUnit("km"),
+          "mi",
+          "return mi for km"
+        );
+        assert.strictEqual(
+          convertHandler.getReturnUnit("lbs"),
+          "kg",
+          "return kg for lbs"
+        );
+        assert.strictEqual(
+          convertHandler.getReturnUnit("kg"),
+          "lbs",
+          "return lbs for kg"
+        );
+      });
+
 });
