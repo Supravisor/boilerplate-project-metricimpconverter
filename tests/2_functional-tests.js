208b60e34be2f688a7c4fb5ac9b6f67d21f3e3e9
Supravisor Supravisor <webtraininghashtag@outlook.com>
2025-05-26 03:45:01 +0000

    Func 1 of 5

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,21 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+    test("GET /api/convert?input=10L", (done) => {
+        chai
+          .request(server)
+          .get("/api/convert?input=10L")
+          .end((req, res) => {
+            assert.equal(res.status, 200);
+            assert.deepEqual(res.body, {
+              initNum: 10,
+              initUnit: "L",
+              returnNum: 2.64172,
+              returnUnit: "gal",
+              string: "10 liters converts to 2.64172 gallons",
+            });
+            done();
+          });
+      });
+    
 });
